376323662a600425c762405aad02ce3d435a3e69
dzonsi <nikola86_v@yahoo.com>
2019-11-12 20:27:17 +0100

    add reactive-forms folder

---
+++ angular/v2/reactive-forms/src/app/profile-editor/profile-editor.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from "@angular/forms";
+import { FormBuilder } from "@angular/forms";
+import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-profile-editor',
+  templateUrl: './profile-editor.component.html',
+  styleUrls: ['./profile-editor.component.css']
+})
+export class ProfileEditorComponent implements OnInit {
+
+	profileForm = this.fb.group({
+		firstName: ["", Validators.required],
+		lastName: [""],
+		address: this.fb.group({
+			street: [""],
+			city: [""],
+			state: [""],
+			zip: [""]
+		}),
+		aliases: this.fb.array([
+			this.fb.control("")
+		])
+	});
+
+	onSubmit() {
+		// TODO: Use EventEmitter with form value
+		console.warn(this.profileForm.value);
+	}
+
+	updateProfile() {
+		this.profileForm.patchValue({
+			firstName: "Aaron Rodgers",
+			address: {
+				street: "Green Bay"
+			}
+		});
+	}
+
+	get aliases() {
+		return this.profileForm.get("aliases") as FormArray;
+	}
+
+	addAlias() {
+		this.aliases.push(this.fb.control(""));
+	}
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+  }
+
+}
